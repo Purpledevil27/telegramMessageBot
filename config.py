253c426a5616184ba48eec370f8a141ad4c57449
PurpleDevil27 <rkrahul2703@gmail.com>
2025-05-09 22:24:03 +0530

    Updated config and added fly.io config

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ import os
 api_id = int(os.getenv('API_ID', 0))
 api_hash = os.getenv('API_HASH', '')
 
-SOURCE_CHANNEL = int(os.getenv('SOURCE_CHANNEL', 0))
-DESTINATION_CHANNEL = os.getenv('DESTINATION_CHANNEL', '')
+source_channel_name = int(os.getenv('SOURCE_CHANNEL', 0))
+destination_channel = os.getenv('DESTINATION_CHANNEL', '')
